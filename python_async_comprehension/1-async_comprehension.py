0ecc4b14e2043c08e3d40419fbd4957674fcfc5e
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-04-23 16:57:56 +0200

    Update 1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 """
-async_Comprehensions module
+async_comprehension
+:return: list of float
 """
 import asyncio
+import random
 from typing import List
+
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """
+    async_comprehension
+    :return: list of float
     """
-    return [number async for number in async_generator()]
+    return [_ async for _ in async_generator()]
