ae3482bb131aefaf1418aa4075ac78bc2b44937d
Fuma1322 <leonardfuma26@gmail.com>
2024-04-23 17:10:09 +0200

    python

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-''' Async Comprehension '''
-from typing import List
-Vector = List[float]
+"""Task 1: Asynchronous Comprehension"""
 
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Vector:
-    ''' Function that return a list '''
-    Final = [y async for y in async_generator()]
-    return Final
+async def async_comprehension() -> List[float]:
+    """Collect 10 random"""
+    random_numbers = [x async for x in async_generator()]
+    return random_numbers
