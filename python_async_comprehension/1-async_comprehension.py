9ae3f79cb9c28a9430df055861cc6de76e2b80e3
Fuma1322 <leonardfuma26@gmail.com>
2024-04-24 10:52:27 +0200

    python

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,25 +3,10 @@
 Contains the async_comprehension coroutine
 """
 
-import asyncio
+import random
 from typing import List
-from random import uniform
 
 async_generator = __import__('0-async_generator').async_generator
 
-
 async def async_comprehension() -> List[float]:
-    """
-    Collects 10 random numbers using an async comprehension over async_generator.
-    """
-    return [i async for i in async_generator()]
-
-
-async def main():
-    """
-    Main coroutine to demonstrate the usage of async_comprehension.
-    """
-    print(await async_comprehension())
-
-if __name__ == "__main__":
-    asyncio.run(main())
+    return [rand async for rand in async_generator()]
