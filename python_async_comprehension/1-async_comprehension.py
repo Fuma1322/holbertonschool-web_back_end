430da0bea30afe555dce577107a602b9aff15e94
Fuma1322 <leonardfuma26@gmail.com>
2024-04-24 10:54:09 +0200

    python

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""
-Contains the async_comprehension coroutine
-"""
+''' Async Comprehension '''
 
-import random
 from typing import List
+Vector = List[float]
 
 async_generator = __import__('0-async_generator').async_generator
 
-async def async_comprehension() -> List[float]:
-    return [rand async for rand in async_generator()]
+
+async def async_comprehension() -> Vector:
+    ''' Function that return a list '''
+    Final = [y async for y in async_generator()]
+    return Final
