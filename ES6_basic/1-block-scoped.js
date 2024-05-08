cb449c2c3664da0f7eb2fc0967a8acaf9fbc4b23
Fuma1322 <leonardfuma26@gmail.com>
2024-05-08 14:38:25 +0200

    es6

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,8 +2,11 @@ export default function taskBlock(trueOrFalse) {
     var task = false;
     var task2 = true;
   
-    // No need to change the values of `task` and `task2` based on `trueOrFalse`
-    // Remove the conditional block that modifies them
+    if (trueOrFalse) {
+        var task = true;
+        var task2 = false;
+      }
+    
   
     return [task, task2];
   }
