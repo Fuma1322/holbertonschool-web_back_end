34404b7a566abe2fd106f71ba5ed4aa1f00453c2
Fuma1322 <leonardfuma26@gmail.com>
2024-05-08 14:58:06 +0200

    es6

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -4,7 +4,7 @@ export default function taskBlock(trueOrFalse) {
 
     if (trueOrFalse) {
         var task = false;
-        let task2 = true;
+        var task2 = true;
     }
         
         return [task, task2];
